d639ef06025675baa90c08d1197a0fafb75c1b1c
Imaginary_Ellen <imaginary.heroes@yandex.ru>
2023-03-18 21:33:15 +0300

    fix: сделать коллекцию history - final

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-	private LinkedList<Task> history = new LinkedList<>();
+	private final LinkedList<Task> history = new LinkedList<>();
 
 	@Override
 	public void add(Task task) {
