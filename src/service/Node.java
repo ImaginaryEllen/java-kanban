3a649368ca119ec1c8825792795fc5207eb3e435
Imaginary_Ellen <imaginary.heroes@yandex.ru>
2023-04-07 19:14:48 +0300

    fix: исправить модификаторы доступа у полей класса, добавить геттеры и сеттеры

--- src/service/Node.java
+++ src/service/Node.java
@@ -3,13 +3,33 @@ package service;
 import model.Task;
 
 class Node {
-	public Task task;
-	public Node next;
-	public Node prev;
+	private Task task;
+	private Node next;
+	private Node prev;
 
 	public Node(Node prev, Task task, Node next) {
 		this.task = task;
 		this.next = next;
+		this.prev = prev;
+	}
+
+	public Task getTask() {
+		return task;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
 		this.prev = prev;
 	}
 }
